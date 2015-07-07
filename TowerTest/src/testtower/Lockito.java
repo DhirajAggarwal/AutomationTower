d7427a7f1821883acc68c78f9328a6c31d41f7eb
DhirajAggarwal <dhirajaggarwal2010@gmail.com>
2015-07-07 19:21:37 +0530

    Demo Automation of SmartDriver Application Using Appium.

--- TowerTest/src/testtower/Lockito.java
+++ TowerTest/src/testtower/Lockito.java
@@ -1,43 +1,51 @@
 package testtower;
 
+import io.appium.java_client.android.AndroidDriver;
+
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 public class Lockito {
-	WebDriver driver;
+
+	AndroidDriver driver;
 
 	@BeforeClass
 	public void setup() throws MalformedURLException {
-		DesiredCapabilities capabilities = new DesiredCapabilities();
-		
-		
-		/*capabilities.setCapability("Platform Version", "4.4");
-		capabilities.setCapability("deviceName", "0cbbdc0a032f9efa");
-		capabilities.setCapability("Platform Name", "Android");
-		capabilities.setCapability("Automation Name", "Appium");
-		capabilities.setCapability("appium-version", "1.4.0.0");*/
 
-		capabilities.setCapability("Package", "fr.dvilleneuve.lockito");
-		// This package name of your app (you can get it from apk info app)
-		//capabilities.setCapability("appActivity",
-		//		"com/example/hello/MainActivity.java");
+		DesiredCapabilities capabilities1 = new DesiredCapabilities();
+		/*
+		 * capabilities .setCapability( "app",
+		 * "C:\\Users\\dhiraj01\\Downloads\\apk-downloader\\fr.dvilleneuve.lockito-2005004.apk"
+		 * );
+		 */
+		capabilities1.setCapability("appPackage", "fr.dvilleneuve.lockito");
+		capabilities1.setCapability("appActivity", ".ui.SplashscreenActivity_");
+		capabilities1.setCapability("deviceName", "0cbbdc0a032f9efa");
+		capabilities1.setCapability("platformName", "Android");
+		capabilities1.setCapability("appWaitActivity",
+				".ui.ItinerariesActivity_");
+
+		driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),
+				capabilities1);
+
+		List<WebElement> Trip_Selection = driver.findElements(By
+				.className("android.widget.RelativeLayout"));
+		Trip_Selection.get(2).click();
+
+		WebElement Play_Trip = driver.findElement(By
+				.id("fr.dvilleneuve.lockito:id/menuPlay"));
+		Play_Trip.click();
+		driver.quit();
 		
-		
-		driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"),
-				capabilities);
+			
 	}
-
-	/*@Test
-	public void test_1x  {
-		
-
-	}*/
 
 	@AfterClass
 	public void afterClass() {
--- TowerTest/src/testtower/NewTest.java
+++ TowerTest/src/testtower/NewTest.java
@@ -1,14 +1,19 @@
 package testtower;
 
+import io.appium.java_client.android.AndroidDriver;
+
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
@@ -17,14 +22,29 @@ import org.testng.annotations.Test;
 
 public class NewTest {
 
-	WebDriver driver;
+	// NewTest screenshot=new NewTest();
+	// WebDriver driver;
+	AndroidDriver driver;
+	String System_time, Latest_Trip_Time;
+
+	DesiredCapabilities capabilities = new DesiredCapabilities();
 
 	@BeforeClass
 	public void setup() throws MalformedURLException {
 
-		DesiredCapabilities capabilities = new DesiredCapabilities();
-		driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"),
-				capabilities);
+		capabilities
+				.setCapability(
+						"app",
+						"C:\\Users\\dhiraj01\\Downloads\\smartdriver-android-1.6-SNAPSHOT[staging644].apk");
+		capabilities.setCapability("appPackage", "com.tower.smartdriver");
+		capabilities.setCapability("appActivity", ".activity.WelcomeActivity");
+		capabilities.setCapability("deviceName", "0cbbdc0a032f9efa");
+		capabilities.setCapability("platformName", "Android");
+		capabilities.setCapability("appWaitActivity",
+				".activity.WelcomeActivity");
+
+		driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),
+				capabilities);// new RemoteWebDriver is Selenium Specific Class.
 	}
 
 	@Test
@@ -62,6 +82,7 @@ public class NewTest {
 	public void test3_NavigateToMenu() throws InterruptedException {
 
 		WebDriverWait wait = new WebDriverWait(driver, 90);
+
 		wait.until(ExpectedConditions.elementToBeClickable(By
 				.id("android:id/alertTitle")));
 
@@ -69,14 +90,27 @@ public class NewTest {
 				.id("android:id/button1"));
 		Continue_button.click();
 
+		wait.until(ExpectedConditions.elementToBeClickable(By
+				.id("com.tower.smartdriver:id/start_trip_btn")));
+
 		WebElement Navigation_button = driver.findElement(By
 				.id("android:id/up"));
 		Navigation_button.click();
 
+		List<WebElement> Navigation_Menu = driver.findElements(By
+				.id("com.tower.smartdriver:id/navigation_item_name"));
+		Navigation_Menu.get(0).click();
+
 	}
 
 	@Test
-	public void test5_NavigateToHome() throws InterruptedException {
+	public void test5_NavigateToHome() throws InterruptedException,
+			MalformedURLException {
+		WebDriverWait wait = new WebDriverWait(driver, 180);
+
+		WebElement Navigation_button = driver.findElement(By
+				.id("android:id/up"));
+		Navigation_button.click();
 
 		List<WebElement> Navigation_Menu = driver.findElements(By
 				.id("com.tower.smartdriver:id/navigation_item_name"));
@@ -86,7 +120,6 @@ public class NewTest {
 				.id("com.tower.smartdriver:id/toggle_button1"));
 		AutoStart_Toggle.click();
 
-		WebDriverWait wait = new WebDriverWait(driver, 90);
 		wait.until(ExpectedConditions.elementToBeClickable(By
 				.id("android:id/message")));
 
@@ -94,108 +127,153 @@ public class NewTest {
 				.id("android:id/button1"));
 		AutoStart_Toggle_Success_Dialog.click();
 
-		WebElement Navigation_button = driver.findElement(By
-				.id("android:id/up"));
 		Navigation_button.click();
-
-		// List<WebElement> Navigation_Menu = driver.findElements(By
-		// .id("com.tower.smartdriver:id/navigation_item_name"));
 		Navigation_Menu.get(0).click();
 
-		// WebDriverWait wait = new WebDriverWait(driver, 180);
-		WebElement Start_buttton = driver.findElement(By
-				.id("com.tower.smartdriver:id/start_trip_btn"));
 		wait.until(ExpectedConditions.elementToBeClickable(By
 				.id("com.tower.smartdriver:id/start_trip_btn")));
 
-		Start_buttton.click();
-		
+		driver.openNotifications();
+		wait.until(
+				ExpectedConditions.elementToBeClickable(By
+						.id("android:id/action0"))).click();
+		driver.navigate().back();
+
+		if (wait.until(ExpectedConditions.visibilityOfElementLocated(By
+				.id("com.tower.smartdriver:id/start_trip_btn"))) != null) {
+
+			driver.findElement(By.id("com.tower.smartdriver:id/start_trip_btn"))
+					.click();
+
+		}
 
 		wait.until(ExpectedConditions.textToBePresentInElement(
 				driver.findElement(By
 						.id("com.tower.smartdriver:id/log_trip_distance_display")),
 				"2.2"));
 
-		WebElement Stop = driver.findElement(By
-				.id("com.tower.smartdriver:id/stop_trip_btn_anim"));
-		Stop.click();
+		/*driver.openNotifications();
+		wait.until(ExpectedConditions.elementToBeClickable(By
+				.id("com.android.systemui:id/backgroundNormal")));
+		List<WebElement> Play_Stop = driver.findElements(By
+				.id("android:id/action0"));
+		Play_Stop.get(1).click();
+		driver.navigate().back();*/
+
+		driver.findElement(By.id("com.tower.smartdriver:id/stop_trip_btn_anim"))
+				.click();
 
 		wait.until(ExpectedConditions.elementToBeClickable(By
 				.id("com.tower.smartdriver:id/distance_driven_label")));
+		// WebElement Navigation_button = driver.findElement(By
+		// .id("android:id/action_bar_title"));
+		Navigation_button.click();
+
+		// List<WebElement> Navigation_Menu = driver.findElements(By
+		// .id("com.tower.smartdriver:id/navigation_item_name"));
+		Navigation_Menu.get(0).click();
+
+	}
+
+	@Test
+	public void test6_NavigateToTripHistory() throws InterruptedException {
+
+		WebElement Navigation_button = driver.findElement(By
+				.id("android:id/action_bar_title"));
+		Navigation_button.click();
+
+		List<WebElement> Navigation_Menu = driver.findElements(By
+				.id("com.tower.smartdriver:id/navigation_item_name"));
+		Navigation_Menu.get(1).click();
+
+		WebDriverWait wait = new WebDriverWait(driver, 90);
+		wait.until(ExpectedConditions.elementToBeClickable(By
+				.id("android:id/list")));
+
+		List<WebElement> Trip_History_Detail = driver.findElements(By
+				.className("android.widget.LinearLayout"));
+		Trip_History_Detail.get(4).click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(By
+				.id("android:id/action_bar_title")));
+
+		driver.navigate().back();
 		driver.navigate().back();
 	}
 
-	/*
-	 * @Test public void test6_NavigateToTripHistory() throws
-	 * InterruptedException {
-	 * 
-	 * //driver.navigate().back(); WebElement Navigation_button =
-	 * driver.findElement(By .id("android:id/up")); Navigation_button.click();
-	 * 
-	 * List<WebElement> Navigation_Menu = driver.findElements(By
-	 * .id("com.tower.smartdriver:id/navigation_item_name"));
-	 * Navigation_Menu.get(1).click();
-	 * 
-	 * WebDriverWait wait = new WebDriverWait(driver, 90);
-	 * wait.until(ExpectedConditions.elementToBeClickable(By
-	 * .id("android:id/list")));
-	 * 
-	 * List<WebElement> Trip_History_Detail = driver.findElements(By
-	 * .className("android.widget.LinearLayout"));
-	 * Trip_History_Detail.get(4).click(); driver.navigate().back();
-	 * driver.navigate().back();
-	 * 
-	 * }
-	 */
-	/*
-	 * @Test public void test7_NavigateToLeaderBoars() throws
-	 * InterruptedException {
-	 * 
-	 * }
-	 * 
-	 * @Test public void test8_NavigateToAchievements() throws
-	 * InterruptedException {
-	 * 
-	 * }
-	 * 
-	 * @Test public void test9_NavigateToAccounts() throws InterruptedException
-	 * {
-	 * 
-	 * }
-	 * 
-	 * @Test public void test10_NavigateToSettings() throws InterruptedException
-	 * {
-	 * 
-	 * List<WebElement> Navigation_Menu = driver.findElements(By
-	 * .id("com.tower.smartdriver:id/navigation_item_name"));
-	 * Navigation_Menu.get(5).click();
-	 * 
-	 * WebElement AutoStart_Toggle = driver.findElement(By
-	 * .id("com.tower.smartdriver:id/toggle_button1"));
-	 * AutoStart_Toggle.click();
-	 * 
-	 * WebDriverWait wait = new WebDriverWait(driver, 90);
-	 * wait.until(ExpectedConditions.elementToBeClickable(By
-	 * .id("android:id/message")));
-	 * 
-	 * WebElement AutoStart_Toggle_Success_Dialog = driver.findElement(By
-	 * .id("android:id/button1")); AutoStart_Toggle_Success_Dialog.click();
-	 * 
-	 * }
-	 * 
-	 * @Test public void test11_NavigateToStartAgain() throws
-	 * InterruptedException {
-	 * 
-	 * }
-	 * 
-	 * @Test public void test12_NavigateToHelp() throws InterruptedException {
-	 * 
-	 * }
-	 * 
-	 * @Test public void test13_Logout() throws InterruptedException {
-	 * 
-	 * }
-	 */
+	@Test
+	public void test7_NavigateToLeaderBoard() throws InterruptedException,
+			IOException {
+
+		WebElement Navigation_button = driver.findElement(By
+				.id("android:id/action_bar_title"));
+		Navigation_button.click();
+
+		List<WebElement> Navigation_Menu = driver.findElements(By
+				.id("com.tower.smartdriver:id/navigation_item_name"));
+		Navigation_Menu.get(2).click();
+
+		WebDriverWait wait = new WebDriverWait(driver, 90);
+
+		Thread.sleep(4000);
+
+		List<WebElement> all_NewZealand = driver.findElements(By
+				.className("android.widget.RelativeLayout"));
+
+		all_NewZealand.get(0).click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(By
+				.id("com.tower.smartdriver:id/pinned_header")));
+
+		File scrFile = ((TakesScreenshot) driver)
+				.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(scrFile, new File("D:\\screenshot.png"));
+
+		driver.navigate().back();
+		driver.navigate().back();
+	}
+
+	@Test
+	public void test8_NavigateToSettings() throws InterruptedException {
+
+		WebDriverWait wait = new WebDriverWait(driver, 90);
+
+		WebElement Navigation_button = driver.findElement(By
+				.id("android:id/action_bar_title"));
+		Navigation_button.click();
+
+		List<WebElement> Navigation_Menu = driver.findElements(By
+				.id("com.tower.smartdriver:id/navigation_item_name"));
+		Navigation_Menu.get(5).click();
+
+		WebElement AutoStart_Toggle = driver.findElement(By
+				.id("com.tower.smartdriver:id/toggle_button1"));
+		AutoStart_Toggle.click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(By
+				.id("android:id/message")));
+
+		WebElement AutoStart_Toggle_Success_Dialog = driver.findElement(By
+				.id("android:id/button1"));
+		AutoStart_Toggle_Success_Dialog.click();
+
+		Navigation_button.click();
+		Navigation_Menu.get(0).click();
+	}
+
+	@Test
+	public void test9_Logout() throws InterruptedException {
+
+		WebElement Navigation_button = driver.findElement(By
+				.id("android:id/action_bar_title"));
+		Navigation_button.click();
+		List<WebElement> Navigation_Menu = driver.findElements(By
+				.id("com.tower.smartdriver:id/navigation_item_name"));
+		Navigation_Menu.get(8).click();
+
+		driver.findElement(By.id("android:id/button1")).click();
+	}
+
 	@AfterClass
 	public void afterClass() {
 	}
